c8052591aa7d846b9c5c32f6c061c36241f350be
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-08-30 12:37:43 +0530

    Time: 156 ms (44.51%), Space: 43.9 MB (30.40%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+/*  0. Keep sum of A[i].
+    1. Check if we have a sum-k in map  ...=> sum+A[i] == k => A[i] == sum-k
+    2. if yes then increment the count
+    3. Add the sum to map.
+*/
+    int subarraySum(vector<int>& nums, int k) {
+        map<int,int> mp;
+        mp.insert(pair<int,int>(0,1));
+        int sum = 0,count = 0;
+        for(int i = 0;i<nums.size();i++){
+            sum += nums[i];
+            if(mp.find(sum-k)->second)count += mp.find(sum-k)->second;
+            mp[sum] = mp[sum]+1;
+        }
+        return count;
+    }
+};
