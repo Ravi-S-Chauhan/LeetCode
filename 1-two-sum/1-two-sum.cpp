fcc73715a2730ba5c525e0eb9f4109abddfcc27b
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-08-30 17:51:26 +0530

    Time: 17 ms (73.82%), Space: 11.2 MB (18.49%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+/*
+    1. We will search for nums[i]-target in map. If found we can directly return it that elements index and current index
+    2. If not found then we can add that number to the map for future use
+*/
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> mp;
         vector<int> result;
