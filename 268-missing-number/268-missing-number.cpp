57ec989ebaa5d60251e4cdfa8f6b29f424b4db10
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-08-30 11:45:37 +0530

    Time: 20 ms (89.69%), Space: 18.1 MB (20.23%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+/*
+1. Xor if same numbers is zeros i.e (1^1 = 0 => 1^1^4 => 4) using this property we find the xor of all the elements with their indexes which will result in zero.
+2. We have an extra element which have occupied the place of missing element.
+3. But that element would be N (size of nums) as one element is missing that we are finding. eg( in [0,1,3] 2 is missing element and 3 has occupied 3's place and the size of the list is 3)
+4. return the solution of xor with ^ of N
+*/
+    
+    int missingNumber(vector<int>& nums) {
+        int N = nums.size(),xr = 0;
+        for (int i = 0;i<N; i++){
+            xr = xr^i^nums[i];
+        }
+        return xr^N;
+    }
+};
