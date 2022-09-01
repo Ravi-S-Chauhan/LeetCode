84988d5ef6b27156e3c1aa12cc1762dd6aab9127
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-01 17:15:33 +0530

    Time: 34 ms (41.77%), Space: 19.6 MB (59.89%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,5 +1,8 @@
 class Solution {
 public:
+/*
+    If count == 0 initialize the ele with current element and count increment to 1 else if curr is same as the ele variable increase the count else if curr is not same as the ele variable decrement count. This works because there always exists the element whose frequency is > n/2
+*/
     int majorityElement(vector<int>& nums) {
         int count=0,ele;
         for(int i = 0;i<nums.size();i++){
@@ -7,9 +10,7 @@ public:
                 ele = nums[i];
                 count++;
             }
-            else if (ele == nums[i]){
-                count++;
-            }
+            else if (ele == nums[i]) count++;
             else count--;
         }
         return ele;
