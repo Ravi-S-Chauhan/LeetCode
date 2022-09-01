cd69d1f0548e067f5b1a9c1db38fbb0acffd85e9
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-01 21:18:39 +0530

    Time: 260 ms (5.13%), Space: 67.8 MB (53.87%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+/*
+    keep suming the elements and if the current sum is greater than max then update the max.
+    if sum < 0 then we can freely remove them as they are only being a dead weight to the maximum sum
+
+*/
+    
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0,mx = -INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            sum+= nums[i];
+            mx = max(mx,sum);
+            if (sum<0) sum = 0;
+        }
+        return mx;
+    }
+    
+};
