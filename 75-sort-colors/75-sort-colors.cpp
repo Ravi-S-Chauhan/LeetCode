63b94c738872bc6a3caa885fe724cff68778db34
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-01 17:26:13 +0530

    Time: 8 ms (11.85%), Space: 8.4 MB (23.75%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+/*
+    3 pointer low,mid(initilaized to 0) and high(N-1) 
+    run a while loop till mid<=high ->1,2,3
+    1 if mid == 1 continue as we want 1 in the middle
+    2 if mid == 0 swap with low and increment both as we want 1     in the begining
+    3. if mid == 2 swap with high and decrement high not the        mid
+    as we have not yet came across high pointers element we         dont want to miss 1 or 2
+*/
     void swap(vector<int>& nums,int i,int j){
         int temp = nums[i];
         nums[i] = nums[j];
@@ -21,3 +29,7 @@ public:
         }
     }
 };
+
+
+
+
