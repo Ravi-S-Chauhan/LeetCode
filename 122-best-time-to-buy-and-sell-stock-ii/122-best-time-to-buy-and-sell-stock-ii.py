755b0a6e4c738c7260831f7bc0626feb30df0094
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-10 16:18:34 +0530

    Time: 90 ms (64.09%), Space: 17.2 MB (6.62%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = [[-1]*(2)for _ in range(len(prices))]
-        return self.solve(prices,0,1,dp)
-    def solve(self,prices,index,buy,dp):
-        if index == len(prices): return 0
-        if dp[index][buy] != -1: return dp[index][buy]
-        profit = 0
-        if buy:
-            profit = max((-prices[index]+self.solve(prices,index+1,0,dp)),(self.solve(prices,index+1,1,dp)))
-        else:
-            profit = max((prices[index]+self.solve(prices,index+1,1,dp)),(self.solve(prices,index+1,0,dp)))
-        dp[index][buy] = profit
-        return profit
+        N = len(prices)
+        dp = [[0]*(2)for _ in range(N+1)]
+        for index in range(N-1,-1,-1):
+            for buy in range(0,2):
+                if index == N:
+                    dp[N][buy] = 1
+                profit = 0
+                if buy:
+                    profit = max((-prices[index]+dp[index+1][0]),(dp[index+1][1]))
+                else:
+                    profit = max((prices[index]+dp[index+1][1]),(dp[index+1][0]))
+                dp[index][buy] = profit
+        return dp[0][1]
+    
+    
+    # def solve(self,prices,index,buy,dp):
+    #     if index == len(prices): return 0
+    #     if dp[index][buy] != -1: return dp[index][buy]
+    #     profit = 0
+    #     if buy:
+    #         profit = max((-prices[index]+self.solve(prices,index+1,0,dp)),(self.solve(prices,index+1,1,dp)))
+    #     else:
+    #         profit = max((prices[index]+self.solve(prices,index+1,1,dp)),(self.solve(prices,index+1,0,dp)))
+    #     dp[index][buy] = profit
+    #     return profit
         
         
         
