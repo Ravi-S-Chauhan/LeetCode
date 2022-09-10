f0ce2a221c2a4414626a2d61ad3a60399ebbb009
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-10 22:59:15 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.06%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.cpp
+++ 650-2-keys-keyboard/650-2-keys-keyboard.cpp
@@ -1,14 +1,28 @@
 class Solution {
 public:
     int minSteps(int n) {
+        if (n<=1) return 0;
         int result;
-        for(int i = 2;i*i<=n;i++){
+        while (n%2 == 0){
+            result += 2;
+            n /= 2;
+        }
+        while(n%3 == 0){
+            result += 3;
+            n /= 3;
+        }
+        
+        for(int i = 5;i*i<=n;i=i+6){
             while(n%i == 0){
                 result += i;
                 n /= i;
             }
+            while(n%(i+2) == 0){
+                result += (i+2);
+                n /= (i+2);
+            }
         }
-        if (n>1){
+        if (n>3){
             result += n;
         }
         return result;
