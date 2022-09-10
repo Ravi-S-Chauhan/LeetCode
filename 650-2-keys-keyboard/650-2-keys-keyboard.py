964ba25d74f0eb5278e6afee2411e53168c523e7
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-10 22:54:47 +0530

    Time: 44 ms (86.99%), Space: 13.9 MB (74.46%) - LeetHub

--- 650-2-keys-keyboard/650-2-keys-keyboard.py
+++ 650-2-keys-keyboard/650-2-keys-keyboard.py
@@ -1,10 +1,19 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         if n<=1: return 0
-        i = 2;answer = 0;
+        i = 5;answer = 0;
+        while n%2 == 0:
+            n //=2
+            answer += 2
+        while n%3 == 0:
+            n //=3
+            answer += 3
         while i*i<=n:
             while(n%i == 0):
                 answer += i
                 n = n//i
-            i+= 1
-        return answer if n<=1 else answer+n
+            while (n%(i+2) == 0):
+                answer += (i+2)
+                n //=(i+2)
+            i+= 6
+        return answer if n<=3 else answer+n
