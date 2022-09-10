99667f75577d7915125d865e61d7daade228cb13
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-10 23:31:34 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (66.40%) - LeetHub

---
+++ 507-perfect-number/507-perfect-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if(num == 1) return false;
+        int result = 1;
+        for(int i = 2;i*i<=num;i++){
+            if(num%i == 0){
+                result += i;
+                if (i != num/i){
+                    result += num/i;
+                }
+            }
+        }
+        return (result == num);
+    }
+};
