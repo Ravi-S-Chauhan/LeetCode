3ccf7744525bb1f8f913926574c8ccecd41fba4a
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-11 21:52:19 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (96.38%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
+        if (n>=0) return solve2(x,n);
+        else return solve(x,n);
+    }
+    double solve2 (double x,int n){
+        double result = 1;
+        while (n>0){
+            if (n&1) result *= x;
+            x *= x;
+            n /= 2;
+        }
+        return result;
+    }
+    double solve(double x,int n){
         if (n == 0) return 1;
-        double temp = myPow(x,n/2);
+        double temp = solve(x,n/2);
         temp *= temp;
         if (!(n&1)) return temp;
         else{
