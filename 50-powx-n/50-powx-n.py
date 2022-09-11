fd323f30725a0c3f97fddeafa272684cf15b4b2e
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-11 21:47:49 +0530

    Time: 42 ms (69.07%), Space: 14 MB (20.08%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,8 +1,20 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if n>=0: return self.solve2(x,n)
+        else:
+            return self.solve(x,n)
+    def solve2(self,x,n):
+        result = 1
+        while n>0:
+            if(n&1):
+                result *= x
+            x *= x
+            n //= 2
+        return result
+    def solve(self,x,n):
         if n == 0: 
             return 1
-        temp = self.myPow(x,int(n/2))
+        temp = self.solve(x,int(n/2))
         temp = temp*temp
         if n%2 == 0:
             return temp
