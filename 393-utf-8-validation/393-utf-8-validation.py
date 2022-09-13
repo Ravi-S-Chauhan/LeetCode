749327685b632412528d0410832bd0b4f0eef734
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-13 12:37:38 +0530

    Time: 271 ms (14.18%), Space: 14.1 MB (70.76%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -3,19 +3,12 @@ class Solution:
         check = 0
         for each in data:
             if check == 0:
-                if each>>7 == 0b0:
-                    check = 0
-                elif each>>5 == 0b110:
-                    check = 1
-                elif each>>4 == 0b1110:
-                    check = 2
-                elif each>>3 == 0b11110:
-                    check = 3
-                else:
-                    return False
+                if each>>7 == 0b0: check = 0
+                elif each>>5 == 0b110: check = 1
+                elif each>>4 == 0b1110: check = 2
+                elif each>>3 == 0b11110: check = 3
+                else: return False
             else:
-                if each>>6 != 0b10:
-                    return False
+                if each>>6 != 0b10: return False
                 check -= 1
-        print(bin(255),check)
         return check == 0
