e22d4aa2e2ba3014b230b21c89f652966c9ea585
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-13 12:36:54 +0530

    Time: 3 ms (49.48%), Space: 47.2 MB (81.86%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+        int check = 0;
+        for(int each:data){
+            if (check == 0){
+                if ((each>>7) == 0b0) check = 0;
+                else if((each>>5) == 0b110) check = 1;
+                else if((each>>4) == 0b1110) check = 2;
+                else if ((each>>3) == 0b11110) check = 3;
+                else return false;
+            }
+            else{
+                if ((each>>6) != 0b10) return false;
+                check--;
+            }
+        }
+        if(check == 0) return true;
+        return false;
+    }
+}
