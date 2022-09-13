7dac209fc2b14ca09fb148bc3f428f4da0d1a79a
Hunter <57064794+Ravi-S-Chauhan@users.noreply.github.com>
2022-09-13 12:33:25 +0530

    Time: 35 ms (15.46%), Space: 14.1 MB (8.48%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,21 +1,21 @@
-class Solution:
-    def validUtf8(self, data: List[int]) -> bool:
-        check = 0
-        for each in data:
-            if check == 0:
-                if each>>7 == 0b0:
-                    check = 0
-                elif each>>5 == 0b110:
-                    check = 1
-                elif each>>4 == 0b1110:
-                    check = 2
-                elif each>>3 == 0b11110:
-                    check = 3
-                else:
-                    return False
-            else:
-                if each>>6 != 0b10:
-                    return False
-                check -= 1
-        print(bin(255),check)
-        return check == 0
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int check = 0;
+        for(auto each:data){
+            if (check == 0){
+                if ((each>>7) == 0b0) check = 0;
+                else if ((each>>5) == 0b110) check = 1;
+                else if ((each>>4) == 0b1110) check = 2;
+                else if ((each>>3) == 0b11110) check = 3;
+                else return false; //For 255
+            }
+            else{
+                if ((each>>6) != 0b10) return false;
+                check--;
+            }
+        }
+        if (check == 0) return true;
+        return false;
+    }
+};
